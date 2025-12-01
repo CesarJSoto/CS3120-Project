3510ecb85eefd40bbdd08b9f7fc67524786ae38e
CesarJSoto <cesarsoto010202@gmail.com>
2025-11-30 17:05:04 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
-import sklearn as classification
 import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 file = 'gen9vgc2025regj-1760.json'
 
-df = pd.read_json(file)
+data = pd.read_json(file)
+print(f"** data has {data.shape[0]} rows and {data.shape[1]} columns **")
 
 
-print(df.head)
+
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 99)
+
